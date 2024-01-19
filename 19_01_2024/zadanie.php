dd339a1811823ca4e3cf5d7f6808ebc7276a7bf9
bobitzz <99447888+bobitzz@users.noreply.github.com>
2024-01-19 12:50:05 +0100

    Update zadanie.php

--- 19_01_2024/zadanie.php
+++ 19_01_2024/zadanie.php
@@ -5,14 +5,14 @@
         if(!isset($_POST[$forms]) || empty($_POST[$forms])) {
             echo "Prosze uzupełnić puste pola";
             break;
-        }
-        if($forms == end($form)) {
-            echo "Rejestracja przebiegła poprawnie <br> Dane ucznia z formularzu <br>";
-            echo "Imię i nazwisko: ".$_POST['name']." ".$_POST['surname']."<br>";
-            echo "Wiek: ".$_POST['age']."<br>";
-            echo "Adres: ".$_POST['address']."<br>";
-            echo "Numer kontaktowy: ".$_POST['number']."<br>";
-            echo "Adres email: ".$_POST['mail']."<br>";
+    }
+    if($forms == end($form)) {
+        echo "Rejestracja przebiegła poprawnie <br> Dane ucznia z formularzu <br>";
+        echo "Imię i nazwisko: ".$_POST['name']." ".$_POST['surname']."<br>";
+        echo "Wiek: ".$_POST['age']."<br>";
+        echo "Adres: ".$_POST['address']."<br>";
+        echo "Numer kontaktowy: ".$_POST['number']."<br>";
+        echo "Adres email: ".$_POST['mail']."<br>";
         }
     }
 ?>
